db77b6097aca2b109941e09d06f2a7ab9d2c774c
Nathan C <32592701+awakedev@users.noreply.github.com>
2019-02-12 13:03:51 +0000

    Initial commit

---
+++ Model/VideoContextFactory.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+
+
+namespace VideoTracker.Model
+{
+    public class VideoContextFactory : IDesignTimeDbContextFactory<VideoTrackerContext>
+    {
+        public VideoTrackerContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<VideoTrackerContext>();
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("VideoTrackerContext"));
+
+            return new VideoTrackerContext(optionsBuilder.Options);
+
+        }
+    }
+}
---
+++ Model/VideoTrackerContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace VideoTracker.Model
+{
+    public class VideoTrackerContext: DbContext
+    {
+        public VideoTrackerContext(DbContextOptions<VideoTrackerContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Video> Videos { get; set; }
+    }
+}
