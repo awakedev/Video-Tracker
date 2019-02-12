919d5fbaf096d689600d87250362257f823de478
Nathan C <32592701+awakedev@users.noreply.github.com>
2019-02-12 15:01:26 +0000

    html

---
+++ Controllers/VideosController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using VideoTracker.Model;
+
+namespace VideoTracker.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VideosController : ControllerBase
+    {
+        private readonly VideoTrackerContext _context;
+
+        public VideosController(VideoTrackerContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Videos
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Video>>> GetVideos()
+        {
+            return await _context.Videos.ToListAsync();
+        }
+
+        // GET: api/Videos/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Video>> GetVideo(int id)
+        {
+            var video = await _context.Videos.FindAsync(id);
+
+            if (video == null)
+            {
+                return NotFound();
+            }
+
+            return video;
+        }
+
+        // PUT: api/Videos/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutVideo(int id, Video video)
+        {
+            if (id != video.ID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(video).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!VideoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Videos
+        [HttpPost]
+        public async Task<ActionResult<Video>> PostVideo(Video video)
+        {
+            _context.Videos.Add(video);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetVideo", new { id = video.ID }, video);
+        }
+
+        // DELETE: api/Videos/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Video>> DeleteVideo(int id)
+        {
+            var video = await _context.Videos.FindAsync(id);
+            if (video == null)
+            {
+                return NotFound();
+            }
+
+            _context.Videos.Remove(video);
+            await _context.SaveChangesAsync();
+
+            return video;
+        }
+
+        private bool VideoExists(int id)
+        {
+            return _context.Videos.Any(e => e.ID == id);
+        }
+    }
+}
---
+++ Model/Seed.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+
+namespace VideoTracker.Model
+{
+    public class Seed
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new VideoTrackerContext(
+                serviceProvider.GetRequiredService<DbContextOptions<VideoTrackerContext>>()))
+            {
+                if (context.Videos.Any())
+                {
+                    return;
+                }
+
+                context.Videos.AddRange(
+                    new Video
+                    {
+                        Title = "Die Hard",
+                        Link = "https://www.youtube.com",
+                        Genre = "Movie",
+                        Watched = true
+                    },
+                    new Video
+                    {
+                        Title = "Die Hard 2",
+                        Link = "https://www.youtube.com",
+                        Genre = "Movie",
+                        Watched = true
+                    },
+                    new Video
+                    {
+                        Title = "Die Hard 3",
+                        Link = "https://www.youtube.com",
+                        Genre = "Movie",
+                        Watched = true
+                    },
+                    new Video
+                    {
+                        Title = "Die Hard 4",
+                        Link = "https://www.youtube.com",
+                        Genre = "Movie",
+                        Watched = true
+                    }
+                 );
+                context.SaveChanges();
+            }
+        }
+    }
+}
